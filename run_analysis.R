12bf3e440f7d6b1ed34ee3e6fa7cc23d9cbabe2c
Sergey Prykhozhij <s.prykhozhij@gmail.com>
2014-07-25 23:35:43 -0300

    R script for the project in Getting & Cleaning Data Course

---
+++ run_analysis.R
@@ -0,0 +1,65 @@
+# You should create one R script called run_analysis.R that does the following. 
+# 1. Merges the training and the test sets to create one data set.
+
+
+# read the data into the file
+
+# test data 
+activity_test <- read.table("y_test.txt")
+subject_test <- read.table("subject_test.txt")
+all_data_test <- read.table("X_test.txt")
+
+# train data
+activity_train <- read.table("y_train.txt")
+subject_train <- read.table("subject_train.txt")
+all_data_train <- read.table("X_train.txt")
+
+###############################
+# MERGING
+###############################
+activity <- vector()
+activity[1:2947] <- activity_test[1:2947,]
+activity[2948:10299] <- activity_train[1:7352,]
+
+subjects <- vector()
+subjects[1:2947] <- subject_test[1:2947,]
+subjects[2948:10299] <- subject_train[1:7352,]
+
+
+dataset <- rbind(all_data_test, all_data_train)
+
+
+# 2. Extracts only the measurements on the mean and standard deviation 
+#    for each measurement.
+select_set <- dataset[,c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,121,122,123,124, 125, 126, 161, 162, 163, 164, 165, 166, 201, 202, 214, 215, 227, 228, 240, 241, 253, 254, 266, 267, 268, 269, 270, 271, 345, 346, 347, 348, 349, 350, 424, 425, 426, 427, 428, 429, 503, 504, 516, 517, 529, 530, 542, 543)]
+
+
+# 3. Uses descriptive activity names to name the activities in the data set
+
+# label activities 
+activity[activity== 1] <- "WALKING"
+activity[activity== 2] <- "WALKING_UPSTAIRS"
+activity[activity== 3] <- "WALKING_DOWNSTAIRS"
+activity[activity== 4] <- "SITTING"
+activity[activity== 5] <- "STANDING"
+activity[activity== 6] <- "LAYING"
+
+# Order of columns: activity -> subject -> data columns
+full_dataset <- cbind(activity, subjects, select_set)
+
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(full_dataset) <- c("activity", "subject", "tBodyAcc-mean()-X", "tBodyAcc-mean()-Y", "tBodyAcc-mean()-Z", "tBodyAcc-std()-X", "tBodyAcc-std()-Y", "tBodyAcc-std()-Z", "tGravityAcc-mean()-X", "tGravityAcc-mean()-Y", "tGravityAcc-mean()-Z", "tGravityAcc-std()-X", "tGravityAcc-std()-Y", "tGravityAcc-std()-Z", "tBodyAccJerk-mean()-X", "tBodyAccJerk-mean()-Y", "tBodyAccJerk-mean()-Z", "tBodyAccJerk-std()-X", "tBodyAccJerk-std()-Y", "tBodyAccJerk-std()-Z", "tBodyGyro-mean()-X", "tBodyGyro-mean()-Y", "tBodyGyro-mean()-Z", "tBodyGyro-std()-X", "tBodyGyro-std()-Y", "tBodyGyro-std()-Z", "tBodyGyroJerk-mean()-X", "tBodyGyroJerk-mean()-Y", "tBodyGyroJerk-mean()-Z", "tBodyGyroJerk-std()-X", "tBodyGyroJerk-std()-Y", "tBodyGyroJerk-std()-Z", "tBodyAccMag-mean()", "tBodyAccMag-std()", "tGravityAccMag-mean()", "tGravityAccMag-std()", "tBodyAccJerkMag-mean()", "tBodyAccJerkMag-std()", "tBodyGyroMag-mean()", "tBodyGyroMag-std()", "tBodyGyroJerkMag-mean()", "tBodyGyroJerkMag-std()", "fBodyAcc-mean()-X", "fBodyAcc-mean()-Y", "fBodyAcc-mean()-Z", "fBodyAcc-std()-X", "fBodyAcc-std()-Y", "fBodyAcc-std()-Z", "fBodyAccJerk-mean()-X", "fBodyAccJerk-mean()-Y", "fBodyAccJerk-mean()-Z", "fBodyAccJerk-std()-X", "fBodyAccJerk-std()-Y", "fBodyAccJerk-std()-Z", "fBodyGyro-mean()-X", "fBodyGyro-mean()-Y", "fBodyGyro-mean()-Z", "fBodyGyro-std()-X", "fBodyGyro-std()-Y", "fBodyGyro-std()-Z", "fBodyAccMag-mean()", "fBodyAccMag-std()", "fBodyBodyAccJerkMag-mean()", "fBodyBodyAccJerkMag-std()", "fBodyBodyGyroMag-mean()", "fBodyBodyGyroMag-std()", "fBodyBodyGyroJerkMag-mean()", "fBodyBodyGyroJerkMag-std()")
+
+# 5. Creates a second, independent tidy data set with the average of 
+#    each variable for each activity and each subject.
+
+# designate two columns as factors
+as.factor(full_dataset$activity)
+as.factor(full_dataset$subject)
+
+# the idea here is to calculate averages for each possible factor combination,
+# which should automatically result in a tidy data frame
+full_aggr <- aggregate(full_dataset, by=list(full_dataset$activity, full_dataset$subject), FUN=mean, na.rm=TRUE)
+final_dataset <- full_aggr[, c(1, 2, 5:70)]
+colnames(final_dataset) <- c("activity", "subject", "tBodyAcc-mean()-X", "tBodyAcc-mean()-Y", "tBodyAcc-mean()-Z", "tBodyAcc-std()-X", "tBodyAcc-std()-Y", "tBodyAcc-std()-Z", "tGravityAcc-mean()-X", "tGravityAcc-mean()-Y", "tGravityAcc-mean()-Z", "tGravityAcc-std()-X", "tGravityAcc-std()-Y", "tGravityAcc-std()-Z", "tBodyAccJerk-mean()-X", "tBodyAccJerk-mean()-Y", "tBodyAccJerk-mean()-Z", "tBodyAccJerk-std()-X", "tBodyAccJerk-std()-Y", "tBodyAccJerk-std()-Z", "tBodyGyro-mean()-X", "tBodyGyro-mean()-Y", "tBodyGyro-mean()-Z", "tBodyGyro-std()-X", "tBodyGyro-std()-Y", "tBodyGyro-std()-Z", "tBodyGyroJerk-mean()-X", "tBodyGyroJerk-mean()-Y", "tBodyGyroJerk-mean()-Z", "tBodyGyroJerk-std()-X", "tBodyGyroJerk-std()-Y", "tBodyGyroJerk-std()-Z", "tBodyAccMag-mean()", "tBodyAccMag-std()", "tGravityAccMag-mean()", "tGravityAccMag-std()", "tBodyAccJerkMag-mean()", "tBodyAccJerkMag-std()", "tBodyGyroMag-mean()", "tBodyGyroMag-std()", "tBodyGyroJerkMag-mean()", "tBodyGyroJerkMag-std()", "fBodyAcc-mean()-X", "fBodyAcc-mean()-Y", "fBodyAcc-mean()-Z", "fBodyAcc-std()-X", "fBodyAcc-std()-Y", "fBodyAcc-std()-Z", "fBodyAccJerk-mean()-X", "fBodyAccJerk-mean()-Y", "fBodyAccJerk-mean()-Z", "fBodyAccJerk-std()-X", "fBodyAccJerk-std()-Y", "fBodyAccJerk-std()-Z", "fBodyGyro-mean()-X", "fBodyGyro-mean()-Y", "fBodyGyro-mean()-Z", "fBodyGyro-std()-X", "fBodyGyro-std()-Y", "fBodyGyro-std()-Z", "fBodyAccMag-mean()", "fBodyAccMag-std()", "fBodyBodyAccJerkMag-mean()", "fBodyBodyAccJerkMag-std()", "fBodyBodyGyroMag-mean()", "fBodyBodyGyroMag-std()", "fBodyBodyGyroJerkMag-mean()", "fBodyBodyGyroJerkMag-std()")
+
